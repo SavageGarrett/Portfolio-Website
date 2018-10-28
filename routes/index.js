83a03ee975c6fcb14957f76b20112a69d9406a04
SavageGarrett <carder.320@osu.edu>
2018-10-28 01:25:47 -0400

    Initial File Sync / Added Ignored Files
    Synchronization of files from local web server to GitHub. Added Ignore for secret key files, node_modules, and the files for the MAC OS X browser, to later be implemented.

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET index page. */
+router.get('/index', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET resume page. */
+router.get('/resume', function(req, res, next) {
+  res.render('about');
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services');
+});
+
+/* GET portfolio page. */
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio');
+});
+
+/* GET blog page. */
+router.get('/blog', function(req, res, next) {
+  res.render('blog');
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+/* GET error page. */
+router.get('/error', function(req, res, next) {
+  res.render('error');
+});
+
+module.exports = router;
